990a1c76fdc6de75b8f5140ebb3a950b7e267e67
Solley007 <suleiman.yahya@aun.edu.ng>
2026-03-05 11:14:12 +0100

    changed sql

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import os
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "ize-hostel-secret-2026"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ize_hostel.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(BASE_DIR, "ize_hostel.db")app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
