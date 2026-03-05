a6989c8361a45338db9777fad619dab6b1f0577a
Solley007 <suleiman.yahya@aun.edu.ng>
2026-03-05 20:50:10 +0100

    added .app

--- app.py
+++ app.py
@@ -373,7 +373,8 @@ def seed():
         db.session.commit()
 
 
+with app.app_context():
+    seed()
+
 if __name__ == "__main__":
-    with app.app_context():
-        seed()
     app.run(debug=True)
